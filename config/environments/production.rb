e229085bbd27db7901d468069a3f07ce52f2b86f
dinhthai2206 <dinhthai2206@gmail.com>
2018-12-27 14:13:11 +0700

    fix modal, credential

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.default_url_options = {host: "localhost", port: 3000}
+  # config.action_mailer.smtp_settings = {
+  #   address:              "smtp.gmail.com",
+  #   port:                 587,
+  #   domain:               "google.com",
+  #   user_name:            "awesome.academy.test@gmail.com",
+  #   password:             "Aa@123456",
+  #   authentication:       'plain',
+  #   enable_starttls_auto: true
+  # }
 end
